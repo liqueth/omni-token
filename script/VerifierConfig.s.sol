53777622b47b98e3300ca59e231c09cb22a04ec1
Paul Reinholdtsen <paul@bitsy.services>
2025-08-29 14:23:26 -0500

    forge fmt

--- script/VerifierConfig.s.sol
+++ script/VerifierConfig.s.sol
@@ -12,10 +12,7 @@ contract VerifierConfigDeploy is Script {
         string memory json = vm.readFile(path);
         bytes memory encodedData = vm.parseJson(json);
         console.log("encodedData length: ", encodedData.length);
-        VerifierConfig.Global memory config = abi.decode(
-            encodedData,
-            (VerifierConfig.Global)
-        );
+        VerifierConfig.Global memory config = abi.decode(encodedData, (VerifierConfig.Global));
         console.log("config.chains.length: ", config.chains.length);
         bytes32 salt = 0x0;
         vm.startBroadcast();
--- src/OmniConfig.sol
+++ src/OmniConfig.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "./interfaces/IOmniConfig.sol";
 
 /// @notice a base class for storing immutable configuration data at a predictable address across chains.
-contract OmniConfig is IOmniConfig{
+contract OmniConfig is IOmniConfig {
     uint256 public immutable version;
     uint256[] internal _chains;
 

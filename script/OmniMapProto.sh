95ed61dc1708d2c3eaa3037d31a424931143f49b
Paul Reinholdtsen <paul@bitsy.services>
2025-08-31 11:21:46 -0500

    Wait longer and more often to retry verification. Move function calls on IOmniMapProto to top.

--- script/OmniMapProto.sh
+++ script/OmniMapProto.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-forge script script/OmniMapProto.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast --verify
+forge script script/OmniMapProto.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast --verify --delay 10 --retries 10
 export OmniMap=$(jq -r '.transactions[0].contractAddress' broadcast/OmniMapProto.s.sol/$CHAIN_ID/run-latest.json)
 forge verify-contract -q --chain $CHAIN_ID --rpc-url $CHAIN_ID --etherscan-api-key $ETHERSCAN_KEY $OmniMap src/OmniMap.sol:OmniMap
--- src/interfaces/IOmniMapProto.sol
+++ src/interfaces/IOmniMapProto.sol
@@ -5,6 +5,28 @@ pragma solidity ^0.8.20;
 /// @notice Deploy clones of the OmniMap protofactory.
 /// @author Paul Reinholdtsen
 interface IOmniMapProto {
+    /// @notice Map a chainId to a local address.
+    struct Entry {
+        uint256 chainId;
+        address local;
+    }
+
+    /// @notice Predict the address of a cloned OmniMap.
+    /// @dev Revert on any of the error conditions described below.
+    /// @param entries The array of chainId/local pairs to choose from.
+    /// @return global The predicted address of the clone.
+    /// @return salt The salt used to create the clone.
+    /// @return local The local address for the current chain.
+    function locate(Entry[] memory entries) external view returns (address global, bytes32 salt, address local);
+
+    /// @notice Create a new OmniMap clone for the current chain if it doesn't already exist.
+    /// @dev Revert if locate does.
+    /// @param entries The array of chainId/local pairs to choose from.
+    /// @return global The predicted address of the clone.
+    /// @return salt The salt used to create the clone.
+    /// @return local The local address for the current chain.
+    function clone(Entry[] memory entries) external returns (address global, bytes32 salt, address local);
+
     /// @notice Revert if someone tries to reinitialize an instance.
     error AlreadyInitialized();
 
@@ -19,26 +41,4 @@ interface IOmniMapProto {
 
     /// @notice Emit when a clone is created.
     event Cloned(address indexed referrer, address indexed local);
-
-    /// @notice Map a chainId to a local.
-    struct Entry {
-        uint256 chainId;
-        address local;
-    }
-
-    /// @notice Predict the address of a cloned OmniMap.
-    /// @dev Revert on any of the error conditions described above.
-    /// @param entries The array of chainId/local pairs to choose from.
-    /// @return global The predicted address of the clone.
-    /// @return salt The salt used to create the clone.
-    /// @return local The local address for the current chain.
-    function locate(Entry[] memory entries) external view returns (address global, bytes32 salt, address local);
-
-    /// @notice Create a new OmniMap clone for the current chain if it doesn't already exist.
-    /// @dev Revert if locate does.
-    /// @param entries The array of chainId/local pairs to choose from.
-    /// @return global The predicted address of the clone.
-    /// @return salt The salt used to create the clone.
-    /// @return local The local address for the current chain.
-    function clone(Entry[] memory entries) external returns (address global, bytes32 salt, address local);
 }

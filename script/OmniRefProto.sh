474853428ad813486ab3127dcb78f7ba608382bf
Paul Reinholdtsen <paul@bitsy.services>
2025-08-30 20:31:27 -0500

    deploy and verify

--- script/OmniRefProto.sh
+++ script/OmniRefProto.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-forge script script/OmniRefProto.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast
+forge script script/OmniRefProto.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast --verify
 export OmniRef=$(jq -r '.transactions[0].contractAddress' broadcast/OmniRefProto.s.sol/$CHAIN_ID/run-latest.json)
 forge verify-contract -q --chain $CHAIN_ID --rpc-url $CHAIN_ID --etherscan-api-key $ETHERSCAN_KEY $OmniRef src/OmniRef.sol:OmniRef

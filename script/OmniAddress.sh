0e560b4b8bedff9c940debac05c46027420f462b
Paul Reinholdtsen <paul@bitsy.services>
2025-09-05 20:12:11 -0700

    Comment out verification in OmniAddress.sh

--- script/OmniAddress.sh
+++ script/OmniAddress.sh
@@ -2,4 +2,4 @@
 
 forge script script/OmniAddress.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast --verify --delay 10 --retries 10
 export OmniAddress=$(jq -r '.transactions[0].contractAddress' broadcast/OmniAddress.s.sol/$CHAIN_ID/run-latest.json)
-forge verify-contract -q --chain $CHAIN_ID --rpc-url $CHAIN_ID --etherscan-api-key $ETHERSCAN_KEY $OmniAddress src/OmniAddress.sol:OmniAddress
+# forge verify-contract -q --chain $CHAIN_ID --rpc-url $CHAIN_ID --etherscan-api-key $ETHERSCAN_KEY $OmniAddress src/OmniAddress.sol:OmniAddress

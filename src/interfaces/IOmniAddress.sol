27c92d7db7f61750bd47ca64add1e53165c76229
Paul Reinholdtsen <paul@bitsy.services>
2025-09-05 20:26:42 -0700

    Refine comments

--- src/interfaces/IOmniAddress.sol
+++ src/interfaces/IOmniAddress.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.20;
 
-/// @notice Map a single predictable contract address to many unique chain local addresses.
+/// @notice Map a single predictable contract address to many chain local addresses.
 /// @author Paul Reinholdtsen
 interface IOmniAddress {
     /// @notice Return the chain‑specific address.
--- src/interfaces/IOmniAddressCloner.sol
+++ src/interfaces/IOmniAddressCloner.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.20;
 
-/// @notice Deploy clones of the OmniAddress protofactory.
+/// @notice Deploy clones of OmniAddress.
 /// @author Paul Reinholdtsen
 interface IOmniAddressCloner {
     /// @notice Map a chainId to a local address.
@@ -12,14 +12,12 @@ interface IOmniAddressCloner {
     }
 
     /// @notice Predict the address of a cloned OmniAddress.
-    /// @dev Revert on any of the error conditions described below.
     /// @param entries The array of chainId/local pairs to choose from.
     /// @return clone_ The predicted address of the clone.
     /// @return salt The salt used to create the clone.
     function cloneAddress(Entry[] memory entries) external view returns (address clone_, bytes32 salt);
 
     /// @notice Create a new OmniAddress clone for the current chain if it doesn't already exist.
-    /// @dev Revert if locate does.
     /// @param entries The array of chainId/local pairs to choose from.
     /// @return clone_ The predicted address of the clone.
     /// @return salt The salt used to create the clone. salt = keccak256(abi.encode(entries));
@@ -29,5 +27,5 @@ interface IOmniAddressCloner {
     error AlreadyInitialized();
 
     /// @notice Emit when a clone is created.
-    event Cloned(address indexed referrer, address indexed local);
+    event Cloned(address indexed clone, address indexed local);
 }

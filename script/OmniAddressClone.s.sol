43abf1e5180425178375091adbfc67ab27d6d2d8
Paul Reinholdtsen <paul@bitsy.services>
2025-09-06 23:03:00 -0700

    OmniAddressClone

---
+++ script/OmniAddressClone.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/OmniAddress.sol";
+
+/**
+ * @notice Deploy the OmniAddress factory/implementation contract.
+ * @dev Usage: forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast
+ */
+contract OmniAddressClone is Script {
+    struct Config {
+        string env;
+        string id;
+        OmniAddress.KeyValue[] keyValues;
+    }
+
+    function run() external {
+        IOmniAddressCloner cloner = IOmniAddressCloner(vm.envAddress("OmniAddress"));
+        string memory path = vm.envString("OmniAddressPath");
+        string memory json = vm.readFile(path);
+        bytes memory raw = vm.parseJson(json);
+        Config memory config = abi.decode(raw, (Config));
+        vm.startBroadcast();
+        (address clone,) = cloner.clone(config.keyValues);
+        vm.stopBroadcast();
+        console2.log("address:", clone);
+    }
+}

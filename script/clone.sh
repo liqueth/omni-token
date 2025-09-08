81697875c8d443a109a37407c8c8e3ad1f26b07a
Paul Reinholdtsen <paul@bitsy.services>
2025-09-08 15:24:26 -0700

    Deploy to BSC testnet

--- script/clone.sh
+++ script/clone.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-source config/testnet.env
 OmniAddressPath=config/$CHAIN_ENV/blocker.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast
 OmniAddressPath=config/$CHAIN_ENV/endpoint.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast
 OmniAddressPath=config/$CHAIN_ENV/executor.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast
 OmniAddressPath=config/$CHAIN_ENV/receiver.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast
 OmniAddressPath=config/$CHAIN_ENV/sender.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast
-
 
 OmniAddressPath=config/$CHAIN_ENV/dvn/bitgo.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast
 OmniAddressPath=config/$CHAIN_ENV/dvn/frax.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast

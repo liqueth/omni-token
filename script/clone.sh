8589336ffb507f2ac4a5881631cfa6204ef7ed10
Paul Reinholdtsen <paul@bitsy.services>
2025-09-08 11:08:21 -0700

    Deploy updated OmniAddress clones.

--- script/clone.sh
+++ script/clone.sh
@@ -9,6 +9,7 @@ OmniAddressPath=config/$CHAIN_ENV/sender.json forge script script/OmniAddressClo
 
 
 OmniAddressPath=config/$CHAIN_ENV/dvn/bitgo.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast
+OmniAddressPath=config/$CHAIN_ENV/dvn/frax.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast
 OmniAddressPath=config/$CHAIN_ENV/dvn/gitcoin.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast
 OmniAddressPath=config/$CHAIN_ENV/dvn/google-cloud.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast
 OmniAddressPath=config/$CHAIN_ENV/dvn/layerzero-labs.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast

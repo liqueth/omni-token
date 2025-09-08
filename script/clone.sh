d16a569b9b53160f7e23d7fdb1bdc3397cbdb1ce
Paul Reinholdtsen <paul@bitsy.services>
2025-09-07 22:21:25 -0700

    send out the clones

--- script/clone.sh
+++ script/clone.sh
@@ -6,3 +6,9 @@ OmniAddressPath=config/$CHAIN_ENV/endpoint.json forge script script/OmniAddressC
 OmniAddressPath=config/$CHAIN_ENV/executor.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast
 OmniAddressPath=config/$CHAIN_ENV/receiver.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast
 OmniAddressPath=config/$CHAIN_ENV/sender.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast
+
+
+OmniAddressPath=config/$CHAIN_ENV/dvn/bitgo.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast
+OmniAddressPath=config/$CHAIN_ENV/dvn/gitcoin.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast
+OmniAddressPath=config/$CHAIN_ENV/dvn/google-cloud.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast
+OmniAddressPath=config/$CHAIN_ENV/dvn/layerzero-labs.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --etherscan-api-key $ETHERSCAN_KEY --broadcast

b84431c2765350f64a1b51fa953a8c4662192e74
Paul Reinholdtsen <paul@bitsy.services>
2025-09-12 20:13:30 -0700

    Make OmniToken prototype immutable so it is available to all clones.

--- src/OmniToken.sol
+++ src/OmniToken.sol
@@ -15,19 +15,17 @@ import "@openzeppelin/contracts/proxy/Clones.sol";
  * @custom:source https://github.com/liqueth/omni-token
  */
 contract OmniToken is OFTUpgradeable, IOmniTokenCloner {
-    address internal _prototype;
-    bytes internal _cloneData;
-    IMessagingConfig internal _appConfig;
+    address public immutable prototype;
+    IMessagingConfig internal immutable _appConfig;
 
     constructor(IMessagingConfig appConfig) OFTUpgradeable(appConfig.endpoint().value()) {
-        _prototype = address(this);
+        prototype = address(this);
         _appConfig = appConfig;
         _disableInitializers();
     }
 
     function __OmniToken_init(Config memory config) public initializer {
         __OFT_init(config.name, config.symbol, config.owner);
-        _prototype = msg.sender;
         uint256[][] memory mints = config.mints;
         for (uint256 i = 0; i < mints.length; i++) {
             uint256 chain = mints[i][0];
@@ -52,34 +50,19 @@ contract OmniToken is OFTUpgradeable, IOmniTokenCloner {
 
     function cloneAddress(Config memory config) public view returns (address token, bytes32 salt) {
         salt = keccak256(abi.encode(config));
-        token = Clones.predictDeterministicAddress(_prototype, salt);
+        token = Clones.predictDeterministicAddress(prototype, salt);
     }
 
     function clone(Config memory config) public returns (address token, bytes32 salt) {
-        if (address(this) != _prototype) {
-            return OmniToken(_prototype).clone(config);
+        if (address(this) != prototype) {
+            return OmniToken(prototype).clone(config);
         }
 
         (token, salt) = cloneAddress(config);
         if (address(token).code.length == 0) {
-            Clones.cloneDeterministic(address(this), salt);
-            OmniToken(address(token)).__OmniToken_init(config);
+            token = Clones.cloneDeterministic(address(this), salt);
+            OmniToken(token).__OmniToken_init(config);
             emit Cloned(config.owner, address(token), config.name, config.symbol);
         }
-    }
-
-    function cloneEncoded(bytes memory cloneData_) public returns (address token, bytes32 salt) {
-        (Config memory config) = abi.decode(cloneData_, (Config));
-        (token, salt) = clone(config);
-    }
-
-    /// @inheritdoc IOmniToken
-    function prototype() external view returns (address) {
-        return _prototype;
-    }
-
-    /// @inheritdoc IOmniToken
-    function cloneData() external view returns (bytes memory) {
-        return _cloneData;
     }
 }
--- src/interfaces/IOmniToken.sol
+++ src/interfaces/IOmniToken.sol
@@ -20,14 +20,6 @@ interface IOmniToken is IERC20Metadata {
     event Cloned(address indexed owner, address indexed token, string name, string symbol);
 
     /**
-     * @notice Initialize a clone with encoded parameters.
-     * @param cloneData_ ABI encoded (address,string,string,uint256[][]) tuple of holder, name, symbol, mints.
-     * @return token The new token clone.
-     * @return salt The salt used for the prediction.
-     */
-    function cloneEncoded(bytes memory cloneData_) external returns (address token, bytes32 salt);
-
-    /**
      * @notice Return the canonical prototype used as both implementation and factory for clone deployments.
      * @dev The returned address is the code-bearing contract that minimal proxies (EIP-1167/OpenZeppelin Clones)
      *      delegate to. Treat it as stateless logic; do not send funds here. Useful for tooling and off-chain
@@ -41,11 +33,4 @@ interface IOmniToken is IERC20Metadata {
      * @return chains Array of EVM chain IDs.
      */
     //function chains() external view returns (uint256[] memory);
-
-    /**
-     * @notice Return the zkBridge clone data used to deploy this token.
-     * @dev Contains the holder, name, symbol, and mints for the token.
-     * @return cloneData The ABI-encoded clone data.
-     */
-    function cloneData() external view returns (bytes memory);
 }

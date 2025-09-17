00cfddd629e42d8aa5c476f7154d3c3547d42c9c
Paul Reinholdtsen <paul@bitsy.services>
2025-09-17 13:17:58 -0700

    update MessagingConfig for io directory

--- script/MessagingConfig.s.sol
+++ script/MessagingConfig.s.sol
@@ -7,13 +7,12 @@ import "../src/MessagingConfig.sol";
 
 /**
  * @notice Deploy the OmniAddress factory/implementation contract.
- * @dev Usage: MessagingConfigPath=config/testnet/messaging.json forge script script/MessagingConfig.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast --verify --delay 10 --retries 10
+ * @dev Usage: IN=io/$CHAIN_ID/messaging.json OUT=io/$CHAIN_ID/MessagingConfig.json forge script script/MessagingConfig.s.sol -f $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast --verify --delay 10 --retries 10
  */
 contract MessagingConfigDeploy is Script {
     function run() external {
-        string memory path = vm.envString("MessagingConfigPath");
-        bytes memory raw = vm.parseJson(vm.readFile(path));
-        IMessagingConfig.Struct memory cfg = abi.decode(raw, (IMessagingConfig.Struct));
+        IMessagingConfig.Struct memory cfg =
+            abi.decode(vm.parseJson(vm.readFile(vm.envString("IN"))), (IMessagingConfig.Struct));
         bytes memory args = abi.encode(cfg);
         bytes memory initCode = abi.encodePacked(type(MessagingConfig).creationCode, args);
 
@@ -28,8 +27,6 @@ contract MessagingConfigDeploy is Script {
             console2.log("already deployed");
         }
 
-        string memory env = vm.envString("CHAIN_ENV");
-        string memory jsonPath = string.concat("./config/", env, "/MessagingConfig.json");
-        vm.writeJson(vm.toString(predicted), jsonPath, ".MessagingConfig");
+        vm.writeJson(vm.toString(predicted), vm.envString("OUT"));
     }
 }

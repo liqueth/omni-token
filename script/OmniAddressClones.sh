77cbf587a24d4ddc78f984a62cae76864a8e18a5
Paul Reinholdtsen <paul@bitsy.services>
2025-09-17 12:01:11 -0700

    io update to OmniAddressClones.sh

--- script/OmniAddressClones.sh
+++ script/OmniAddressClones.sh
@@ -6,8 +6,8 @@ Usage:
 script/OmniAddressClones.sh
 '
 
-OmniAddressPath=config/$CHAIN_ENV/blocker.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast
-OmniAddressPath=config/$CHAIN_ENV/endpoint.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast
-OmniAddressPath=config/$CHAIN_ENV/executor.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast
-OmniAddressPath=config/$CHAIN_ENV/receiver.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast
-OmniAddressPath=config/$CHAIN_ENV/sender.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast
+CLN=io/$CHAIN_ID/OmniAddress.json IN=io/testnet/blocker.json OUT=io/$CHAIN_ID/messaging.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast
+CLN=io/$CHAIN_ID/OmniAddress.json IN=io/testnet/endpoint.json OUT=io/$CHAIN_ID/messaging.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast
+CLN=io/$CHAIN_ID/OmniAddress.json IN=io/testnet/executor.json OUT=io/$CHAIN_ID/messaging.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast
+CLN=io/$CHAIN_ID/OmniAddress.json IN=io/testnet/receiver.json OUT=io/$CHAIN_ID/messaging.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast
+CLN=io/$CHAIN_ID/OmniAddress.json IN=io/testnet/sender.json OUT=io/$CHAIN_ID/messaging.json forge script script/OmniAddressClone.s.sol --rpc-url $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast

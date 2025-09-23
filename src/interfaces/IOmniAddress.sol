f76c30880569b69fa385913551a7441cff4ff1af
Paul Reinholdtsen <paul@bitsy.services>
2025-09-23 16:26:35 -0700

    fix typos

--- src/interfaces/IOmniAddress.sol
+++ src/interfaces/IOmniAddress.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.20;
 
 /// @notice Map a single predictable contract address to a chain‑specific address.
-/// @dev This deterministic lookup pattern enables the creation of immutable dependent contracts
-/// at deterministic addresses even if they depend on intialization data that varies by chain.
+/// @dev This deterministic lookup pattern enables the creation of immutable contracts
+/// at deterministic addresses even if they depend on initialization data that varies by chain.
 /// @author Paul Reinholdtsen (reinholdtsen.eth)
 interface IOmniAddress {
     /// @return local address for the current chain.

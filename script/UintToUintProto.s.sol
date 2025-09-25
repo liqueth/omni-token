efe337c63bec823b043aa643752ef5414d7a7839
Paul Reinholdtsen <paul@bitsy.services>
2025-09-24 17:29:19 -0700

    refactor: update deployment script to use 'proto' variable for UintToUintProto

--- script/UintToUintProto.s.sol
+++ script/UintToUintProto.s.sol
@@ -7,7 +7,7 @@ import "../src/ImmutableUintToUint.sol";
 
 /**
  * @notice Deploy the ImmutableUintToUint protofactory contract.
- * @dev Usage: OUT=io/$CHAIN_ID/UintToUintProto.json forge script script/UintToUintProto.s.sol -f $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast --verify --delay 10 --retries 10
+ * @dev Usage: proto=io/$CHAIN_ID/UintToUintProto.json forge script script/UintToUintProto.s.sol -f $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast --verify --delay 10 --retries 10
  */
 contract UintToUintProto is Script {
     function run() external {
@@ -22,6 +22,6 @@ contract UintToUintProto is Script {
             console.log("already deployed");
         }
 
-        vm.writeJson(vm.toString(predicted), vm.envString("OUT"));
+        vm.writeJson(vm.toString(predicted), vm.envString("proto"));
     }
 }

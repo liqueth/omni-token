9b1526ef19790a85ba79041e0ff16e3c4fb4d2ff
Paul Reinholdtsen <paul@bitsy.services>
2025-09-24 18:10:12 -0700

    refactor: remove AddressLookupClones script and integrate its logic into all_deployments.sh

--- script/all_deployments.sh
+++ script/all_deployments.sh
@@ -10,7 +10,13 @@
 script/prep_chain.sh
 proto=io/$CHAIN_ID/AddressLookupProto.json forge script script/AddressLookupProto.s.sol -f $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast --verify --delay 10 --retries 10
 proto=io/$CHAIN_ID/UintToUintProto.json forge script script/UintToUintProto.s.sol -f $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast --verify --delay 10 --retries 10 ## --priority-gas-price 1gwei --with-gas-price 30gwei
-script/AddressLookupClones.sh
+
+CLN=io/$CHAIN_ID/AddressLookupProto.json IN=io/testnet/blocker.json OUT=io/$CHAIN_ID/messaging.json forge script script/AddressLookupClone.s.sol -f $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast
+CLN=io/$CHAIN_ID/AddressLookupProto.json IN=io/testnet/endpoint.json OUT=io/$CHAIN_ID/messaging.json forge script script/AddressLookupClone.s.sol -f $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast
+CLN=io/$CHAIN_ID/AddressLookupProto.json IN=io/testnet/executor.json OUT=io/$CHAIN_ID/messaging.json forge script script/AddressLookupClone.s.sol -f $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast
+CLN=io/$CHAIN_ID/AddressLookupProto.json IN=io/testnet/receiver.json OUT=io/$CHAIN_ID/messaging.json forge script script/AddressLookupClone.s.sol -f $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast
+CLN=io/$CHAIN_ID/AddressLookupProto.json IN=io/testnet/sender.json OUT=io/$CHAIN_ID/messaging.json forge script script/AddressLookupClone.s.sol -f $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast
+
 proto=io/$CHAIN_ID/UintToUintProto.json config=io/testnet/endpointMapper.json clone=io/$CHAIN_ID/messaging.json forge script script/UintToUintClone.s.sol -f $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast
 IN=io/$CHAIN_ID/messaging.json OUT=io/$CHAIN_ID/MessagingConfig.json forge script script/MessagingConfig.s.sol -f $CHAIN_ID --private-key $DEPLOYER_KEY --broadcast --verify --delay 10 --retries 10
 script/deployOmniToken.sh

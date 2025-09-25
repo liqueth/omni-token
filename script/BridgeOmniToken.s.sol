e94100f1da59287973c89089ee20d2dceb2fd496
Paul Reinholdtsen <paul@bitsy.services>
2025-09-24 22:36:40 -0700

    refactor: enhance logging and structure in BridgeOmniToken script

--- script/BridgeOmniToken.s.sol
+++ script/BridgeOmniToken.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
 import {IOmniTokenBridger} from "../src/interfaces/IOmniTokenBridger.sol";
+import {MessagingReceipt, OFTReceipt} from "@layerzerolabs/oft-evm/contracts/oft/interfaces/IOFT.sol";
 
 /// @notice Bridge tokens to another chain.
 /// @dev Environment variables (required):
@@ -18,17 +19,26 @@ contract BridgeOmniToken is Script {
     }
 
     function run() external {
-        IOmniTokenBridger token =
-            IOmniTokenBridger(abi.decode(vm.parseJson(vm.readFile(vm.envString("token"))), (address)));
-        console2.log("token: ", address(token));
+        console2.log("script : BridgeOmniToken");
+        address token = abi.decode(vm.parseJson(vm.readFile(vm.envString("token"))), (address));
+        console2.log("token  :", address(token));
         Input memory input = abi.decode(vm.parseJson(vm.readFile(vm.envString("transfer"))), (Input));
-        console2.log("toChain: ", input.toChain);
-        console2.log("amount: ", input.amount);
+        console2.log("amount :", input.amount);
+        console2.log("toChain:", input.toChain);
 
-        uint256 fee = token.bridgeFee(input.toChain, input.amount);
-        console2.log("fee: ", fee);
+        uint256 fee = IOmniTokenBridger(token).bridgeFee(input.toChain, input.amount);
+        console2.log("fee    :", fee);
         vm.startBroadcast();
-        token.bridge{value: fee}(input.toChain, input.amount);
+        (MessagingReceipt memory msgRct, OFTReceipt memory oftRct) =
+            IOmniTokenBridger(token).bridge{value: fee}(input.toChain, input.amount);
         vm.stopBroadcast();
+
+        console2.log("msgRct.fee.nativeFee   :", msgRct.fee.nativeFee);
+        console2.log("msgRct.fee.lzTokenFee  :", msgRct.fee.lzTokenFee);
+        console2.log("msgRct.guid            :");
+        console2.logBytes32(msgRct.guid);
+        console2.log("msgRct.nonce           :", msgRct.nonce);
+        console2.log("oftRct.amountSentLD    :", oftRct.amountSentLD);
+        console2.log("oftRct.amountReceivedLD:", oftRct.amountReceivedLD);
     }
 }

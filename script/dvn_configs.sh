be461c0b20f6512c6dc0df600f9dfb74a1ec3958
Paul Reinholdtsen <paul@bitsy.services>
2025-09-24 17:48:16 -0700

    refactor: remove deprecated scripts and consolidate deployment logic for UintToAddress

--- script/dvn_configs.sh
+++ script/dvn_configs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Generate config data for all the dvns for a specified environment
-# Usage: script/dvn.sh < config/$CHAIN_ENV/dvns.txt
+# Usage: script/dvn_configs.sh < config/$CHAIN_ENV/dvns.txt
 
 while read dvn; do
     jq --arg env $CHAIN_ENV --arg id $dvn -f jq/dvn.jq io/nickmeta.json > io/$CHAIN_ENV/dvn/$dvn.json

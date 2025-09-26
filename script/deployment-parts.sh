f33b01a00049188dd8f4806dd2631e5a20e59e0f
Paul Reinholdtsen <paul@bitsy.services>
2025-09-26 11:00:33 -0700

    delete unused file

--- script/deployment-parts.sh
+++
@@ -1,6 +0,0 @@
-jq --arg env $env -f jq/blocker.jq io/$env/deployments.json > io/$env/blocker.json
-jq --arg env $env -f jq/endpointMapper.jq io/$env/deployments.json > io/$env/endpointMapper.json
-jq --arg env $env -f jq/endpoint.jq io/$env/deployments.json > io/$env/endpoint.json
-jq --arg env $env -f jq/executor.jq io/$env/deployments.json > io/$env/executor.json
-jq --arg env $env -f jq/receiver.jq io/$env/deployments.json > io/$env/receiver.json
-jq --arg env $env -f jq/sender.jq io/$env/deployments.json > io/$env/sender.json

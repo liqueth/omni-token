d350519d0b526385be5fbcace3010a78185eed2b
Paul Reinholdtsen <paul@bitsy.services>
2025-09-28 18:37:15 -0700

    Fix OmniToken.sh to write to mainnet

--- script/OmniToken.sh
+++ script/OmniToken.sh
@@ -9,4 +9,4 @@
 # chain=97 # BSC Testnet for example
 
 proto=io/$chain/OmniTokenProto.json config=io/$chain/MessagingConfig.json forge script script/OmniTokenProto.s.sol -f $chain --private-key $tx_key --broadcast --verify --delay 10 --retries 10
-proto=io/$chain/OmniTokenProto.json config=io/testnet/OMNIA.json clone=io/$chain/OMNIA.json forge script script/OmniTokenClone.s.sol -f $chain --private-key $tx_key --broadcast
+proto=io/$chain/OmniTokenProto.json config=io/$env/OMNIA.json clone=io/$chain/OMNIA.json forge script script/OmniTokenClone.s.sol -f $chain --private-key $tx_key --broadcast

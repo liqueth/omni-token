a67b29620a36ff631eefdd27cae8e767321bb920
Paul Reinholdtsen <paul@bitsy.services>
2025-10-01 12:08:49 -0700

    OmniTokenProto uses IOmniTokenProtoFactory

--- script/OmniTokenProto.s.sol
+++ script/OmniTokenProto.s.sol
@@ -2,23 +2,24 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import "../src/OmniToken.sol";
+import "../src/interfaces/IOmniTokenProtoFactory.sol";
+import "../src/interfaces/IMessagingConfig.sol";
 
 /// @title Deploy Script for OmniToken
 /// @notice Deploys the OmniToken contract with specified configuration
-/// @dev Usage: config=io/$chain/MessagingConfig.json proto=io/$chain/OmniTokenProto.json forge script script/OmniTokenProto.s.sol -f $chain --private-key $tx_key --broadcast --verify --delay 10 --retries 10
+/// @dev Usage: factory=io/$chain/OmniTokenProtoFactory.json config=io/$chain/MessagingConfig.json proto=io/$chain/OmniTokenProto.json forge script script/OmniTokenProto.s.sol -f $chain --private-key $tx_key --broadcast --verify --delay 10 --retries 10
 contract OmniTokenProto is Script {
     function run() external {
         console2.log("script   : OmniTokenProto");
+        address factory = abi.decode(vm.parseJson(vm.readFile(vm.envString("factory"))), (address));
+        console2.log("factory  : ", factory);
         address config = abi.decode(vm.parseJson(vm.readFile(vm.envString("config"))), (address));
         console2.log("config   : ", config);
-        bytes memory args = abi.encode(config);
-        bytes memory initCode = abi.encodePacked(type(OmniToken).creationCode, args);
-        address predicted = vm.computeCreate2Address(0x0, keccak256(initCode));
+        address predicted = IOmniTokenProtoFactory(factory).createAddress(IMessagingConfig(config));
         console2.log("predicted: ", predicted);
         if (predicted.code.length == 0) {
             vm.startBroadcast();
-            OmniToken actual = new OmniToken{salt: 0x0}(IMessagingConfig(config));
+            address actual = IOmniTokenProtoFactory(factory).create(IMessagingConfig(config));
             vm.stopBroadcast();
             console2.log("actual   : ", address(actual));
         } else {

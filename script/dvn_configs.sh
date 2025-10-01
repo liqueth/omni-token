3c3d1458cd8a6d822898c35e463d02767969e150
Paul Reinholdtsen <paul@bitsy.services>
2025-09-30 21:59:50 -0700

    run dvn_configs.sh

--- script/dvn_configs.sh
+++ script/dvn_configs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Generate config data for all the dvns for a specified environment
-# Usage: script/dvn_configs.sh < config/$env/dvns.txt
+# Usage: script/dvn_configs.sh < io/$env/dvns.txt
 
 while read dvn; do
     jq --arg env $env --arg id $dvn -f jq/dvn.jq io/nickmeta.json > io/$env/dvn/$dvn.json

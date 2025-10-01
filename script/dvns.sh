0b1871f82869f1c6269074912901d7b9fa808240
Paul Reinholdtsen <paul@bitsy.services>
2025-10-01 00:40:08 -0700

    Fix dvns.sh

--- script/dvns.sh
+++ script/dvns.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Deploy DVN configs for all the dvns for a specified environment
-# Usage: script/UintToAddressProto.sh < config/$env/dvns.txt
+# Usage: script/dvns.sh < io/$env/dvns.txt
 
 while read dvn; do
     proto=io/$chain/AddressLookupProto.json config=io/$env/dvn/$dvn.json clone=io/$chain/dvn/$dvn.json forge script script/AddressLookupClone.s.sol -f $chain --private-key $tx_key --broadcast

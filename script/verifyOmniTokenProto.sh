711f860910b66279f08b6b9c880ff6d74c67a322
Paul Reinholdtsen <paul@bitsy.services>
2025-10-15 13:57:12 -0500

    Support manual verification

--- script/verifyOmniTokenProto.sh
+++ script/verifyOmniTokenProto.sh
@@ -1,3 +1,3 @@
 contract=$(jq -r '.transactions[].additionalContracts[].address' broadcast/OmniTokenProto.s.sol/$chain/run-latest.json)
 args=$(cast abi-encode "constructor(address)" $(jq -r '.transactions[].arguments[0]' broadcast/OmniTokenProto.s.sol/$chain/run-latest.json))
-forge verify-contract $contract OmniToken --constructor-args $args --chain $chain
+forge verify-contract $contract OmniToken --constructor-args $args --chain $chain --verifier etherscan # --show-standard-json-input > io/$chain/verify.json

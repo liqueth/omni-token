925474de70f72847fd58a1f39c4f15fdf6142e33
Paul Reinholdtsen <paul@bitsy.services>
2025-10-24 17:04:37 -0500

    feat: add deployment and verification scripts for OFTMinterProtoFactory and OFTMinterProto

---
+++ script/OFTMinterProtoFactory.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Deploy OFTMinterProtoFactory and OFTMinterProto clone
+# prerequesite before running:
+# source config/testnet.env
+# or
+# source config/testnet.env
+# script/chain.sh
+# chain=97 # BSC Testnet for example
+
+factory=io/$chain/OFTMinterProtoFactory.json forge script script/OFTMinterProtoFactory.s.sol -f $chain --private-key $tx_key --broadcast --verify --delay 10 --retries 10
+proto=io/$chain/OFTMinterProto.json factory=io/$chain/OFTMinterProtoFactory.json config=io/$chain/MessagingConfig.json forge script script/OFTMinterProto.s.sol -f $chain --private-key $tx_key --broadcast --verify --delay 10 --retries 10
+script/verifyOFTMinterDeterministic.sh

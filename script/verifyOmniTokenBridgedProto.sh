02027f2c6ec258cbc604771c1526913f1a54f7b3
Paul Reinholdtsen <paul@bitsy.services>
2025-10-27 15:44:00 -0500

    feat: update verification script to handle constructor arguments and improve contract address extraction

--- script/verifyOmniTokenBridgedProto.sh
+++ script/verifyOmniTokenBridgedProto.sh
@@ -1,3 +1,7 @@
-contract=$(jq -r '.transactions[].additionalContracts[].address' broadcast/OmniTokenBridgedProto.s.sol/$chain/run-latest.json)
-args=$(cast abi-encode "constructor(address,address)" $(jq -r '.transactions[0].arguments[0], .transactions[0].arguments[1]' broadcast/OmniTokenBridgedProto.s.sol/$chain/run-latest.json))
+contract=$(jq -r '.transactions[].additionalContracts[0].address' broadcast/OmniTokenBridgedProto.s.sol/$chain/run-latest.json)
+args=$(cast abi-encode \
+  "constructor((address,uint[][],string,address,uint128,string,address),address)" \
+  "$(jq -r '.transactions[0].arguments[0]' broadcast/OmniTokenBridgedProto.s.sol/$chain/run-latest.json)" \
+  "$(jq -r '.transactions[0].arguments[1]' broadcast/OmniTokenBridgedProto.s.sol/$chain/run-latest.json)" \
+)
 forge verify-contract $contract OmniTokenBridged --constructor-args $args --chain $chain --verifier etherscan # --show-standard-json-input > io/$chain/verify.json
